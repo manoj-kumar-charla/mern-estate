be9b6b7465d0591fb0c89ca246240991bae53654
manoj kumar <charlamanojkumar3@gmail.com>
2024-08-05 21:48:06 +0530

    create user model

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function Header() {
-  
+
   return (
     <header className=" bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -40,6 +40,7 @@ function Header() {
         </ul>
       </div>
     </header>
+    
   );
 }
 
