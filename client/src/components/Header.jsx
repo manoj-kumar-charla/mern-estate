3f8c9355e4a1804bfec10f00d54b6269a7716883
manoj kumar <charlamanojkumar3@gmail.com>
2024-08-05 17:33:13 +0530

    create api and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,6 +3,7 @@ import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function Header() {
+  
   return (
     <header className=" bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
