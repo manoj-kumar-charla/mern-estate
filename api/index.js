4c9e58b6768c1c96f0a66908afb5385da1bf6f2f
manoj kumar <charlamanojkumar3@gmail.com>
2024-08-06 01:16:22 +0530

    try to resolve userName error

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ import { log } from 'console';
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,9 +15,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.listen(3000, () => {
   console.log("listening on port 3000 !!");
 })
 
 app.use("/api/user", userRouter);
+app.use("/api/user", authRouter);
